c6f8604947f1f11684aec7b6701b0bc59d1b2707
iniferdooo <127367884+iniferdooo@users.noreply.github.com>
2023-06-08 09:44:22 +0700

    Add files via upload

---
+++ DFS.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modul6latihan;
+
+/**
+ *
+ * @author ASUS
+ */
+import java.util.*;
+
+public class DFS {
+
+    private LinkedList<Integer> adj[];
+    private boolean visited[];
+
+    DFS(int v) {
+        adj = new LinkedList[v];
+        visited = new boolean[v];
+
+        for (int i = 0; i < v; i++) {
+            adj[i] = new LinkedList<Integer>();
+        }
+    }
+
+    void insertEdge(int src, int dest) {
+        adj[src].add(dest);
+    }
+
+    void DFS(int vertex) {
+        visited[vertex] = true;
+        System.out.print(vertex + " ");
+
+        Iterator<Integer> it = adj[vertex].listIterator();
+        while (it.hasNext()) {
+            int n = it.next();
+            if (!visited[n]) {
+                DFS(n);
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        DFS graph = new DFS(8);
+        graph.insertEdge(0, 1);
+        graph.insertEdge(0, 2);
+        graph.insertEdge(0, 3);
+        graph.insertEdge(1, 3);
+        graph.insertEdge(2, 4);
+        graph.insertEdge(3, 5);
+        graph.insertEdge(3, 6);
+        graph.insertEdge(4, 7);
+        graph.insertEdge(4, 5);
+        graph.insertEdge(5, 2);
+
+        System.out.println("Depth First Traversal for the graph is:");
+        graph.DFS(0);
+    }
+}
